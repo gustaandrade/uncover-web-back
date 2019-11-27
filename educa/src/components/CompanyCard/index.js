b6711665e224baefb198b6f105842dce1459a941
Gustavo Andrade <gustavo.andrade@eshows.com.br>
2019-11-27 09:49:23 -0300

    Update CompanyCard Component

--- educa/src/components/CompanyCard/index.js
+++ educa/src/components/CompanyCard/index.js
@@ -4,55 +4,18 @@ import Button from 'react-bootstrap/Button';
 
 import './company-card.css';
 
-import Logo from '../../assets/nubank.jpg';
-
-const CompanyCard = () => {
+const CompanyCard = props => {
   return (
     <>
       <div className="card-deck">
         <Card className="bg-dark text-white">
-          <Card.Img src={Logo} alt="Card image" />
+          <Card.Img src={props.logo} alt="Card image" />
           <Card.ImgOverlay>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>Last updated 3 mins ago</Card.Text>
+            <Card.Title>{props.name}</Card.Title>
+            <Card.Text>{props.data}</Card.Text>
             <Button variant="danger" size="sm" block>Excluir</Button>
           </Card.ImgOverlay>
         </Card>
-        {/* <Card className="bg-dark text-white">
-          <Card.Img src={Logo} alt="Card image" />
-          <Card.ImgOverlay>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>Last updated 3 mins ago</Card.Text>
-          </Card.ImgOverlay>
-        </Card>
-        <Card className="bg-dark text-white">
-          <Card.Img src={Logo} alt="Card image" />
-          <Card.ImgOverlay>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>Last updated 3 mins ago</Card.Text>
-          </Card.ImgOverlay>
-        </Card>
-        <Card className="bg-dark text-white">
-          <Card.Img src={Logo} alt="Card image" />
-          <Card.ImgOverlay>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>Last updated 3 mins ago</Card.Text>
-          </Card.ImgOverlay>
-        </Card>
-        <Card className="bg-dark text-white">
-          <Card.Img src={Logo} alt="Card image" />
-          <Card.ImgOverlay>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>Last updated 3 mins ago</Card.Text>
-          </Card.ImgOverlay>
-        </Card>
-        <Card className="bg-dark text-white">
-          <Card.Img src={Logo} alt="Card image" />
-          <Card.ImgOverlay>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>Last updated 3 mins ago</Card.Text>
-          </Card.ImgOverlay>
-        </Card> */}
       </div>
     </>
   );
